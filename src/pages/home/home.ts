d20bcd39337802a566d9541d4e3c040b5bdeecbb
Your Nameitptnabdularsi <azeed1991@gmail.com>
2018-11-09 14:38:11 +0700

    send cpmponent

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController,ActionSheetController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController,
+    public actionSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
   }
 
+  showAction(){
+    const actionSheet = this.actionSheet.create({
+      title: 'Test Action',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  showAlert(){
+    const alert = this.alertCtrl.create({
+    title:'Confirm Answer',
+    subTitle:'arsi Binkasem',
+    buttons: ['OK']  
+  });
+  alert.present();
+}
 }
